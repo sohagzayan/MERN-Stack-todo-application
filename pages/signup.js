2dc3c5390ead09fb0309ec29c7cd4619885e26bd
gdsohag360 <gdsohag360@gmail.com>
2022-07-11 23:35:06 -0700

    fix class to className

--- pages/signup.js
+++ pages/signup.js
@@ -9,11 +9,11 @@ const Signup = () => {
                            <div className="bg-light_white shadow-md md:w-[50%] w-[90%] mx-auto mt-16 p-8 rounded-md text-center">
                            <form className="flex flex-col items-center justify-center">
                                 <h2 className="font-bold mb-3 text-xl">Sign Up</h2>
-                                <input type="email" placeholder="User Email" class="input input-bordered w-full mb-3" />
+                                <input type="email" placeholder="User Email" className="input input-bordered w-full mb-3" />
                                 <input type="text" placeholder="Fast Name" class="input input-bordered w-full mb-3" />
-                                <input type="text" placeholder="Last Name" class="input input-bordered w-full mb-3" />
-                                <input type="number" placeholder="Mobile" class="input input-bordered w-full mb-3" />
-                                <input type="password" placeholder="User Password" class="input input-bordered w-full mb-3" />
+                                <input type="text" placeholder="Last Name" className="input input-bordered w-full mb-3" />
+                                <input type="number" placeholder="Mobile" className="input input-bordered w-full mb-3" />
+                                <input type="password" placeholder="User Password" className="input input-bordered w-full mb-3" />
                                 <button className="btn bg-dark_purple border-none w-full mb-4 ">Next</button>
                             </form>
                             <span className="block font-bold text-xl">
